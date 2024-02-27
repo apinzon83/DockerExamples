d813a1dc566389a1b8f3d704c6a2c702a40b76de
apinzon83 <apinzon@outlook.com>
2024-02-26 18:13:41 -0600

    segundo commit

---
+++ InfiniteCounter/infinite-counter.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Hola!"
+echo "he depertardo"
+
+i=0
+
+while :
+do
+	echo "sigo despierto, i=$i"
+	((i=i+1))
+	sleep 1
+done
---
+++ SimpleGreeting/simple-greeting.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+read -p "Cual es tu nombre? " name
+echo "Hola $name"
